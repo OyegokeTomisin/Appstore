3a9a38c2b6f6ba72b2db15b83d61485268505b22
Oyegoke Tomisin <tomisin18@gmail.com>
2019-04-03 14:30:52 +0100

    First Commit

---
+++ Appstore/Controller/BaseTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseTabBarController.swift
+//  Appstore
+//
+//  Created by OYEGOKE TOMISIN on 01/04/2019.
+//  Copyright © 2019 com.OyegokeTomisin. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let today = createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon")
+        let apps = createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps")
+        let search = createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
+        viewControllers = [today, apps, search]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UINavigationController{
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.view.backgroundColor = .white
+        viewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
+    }
+    
+}
---
+++ Appstore/Views/SearchResultsCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultsCell.swift
+//  Appstore
+//
+//  Created by OYEGOKE TOMISIN on 01/04/2019.
+//  Copyright © 2019 com.OyegokeTomisin. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsCell: UICollectionViewCell {
+    
+}
