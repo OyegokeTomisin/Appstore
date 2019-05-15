c979bea1a12fd44347e5e3dc7dac94f464a1a069
Oyegoke Tomisin <tomisin18@gmail.com>
2019-05-15 15:51:02 +0100

    Synchronizing Multiple Fetches using Dispatch Group

--- Appstore/Controller/Apps/AppsPageController.swift
+++ Appstore/Controller/Apps/AppsPageController.swift
@@ -21,19 +21,56 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
         fetchData()
     }
     
-    var editorsChoiceGames: AppGroup?
-    
+    //var editorsChoiceGames: AppGroup?
+    var groups = [AppGroup]()
     fileprivate func fetchData(){
-        Service.shared.fetchGames { (appGroup, err) in
-            if let err = err{
-                print(err)
-            }
-            self.editorsChoiceGames = appGroup
+        
+        var group1: AppGroup?
+        var group2: AppGroup?
+        var group3: AppGroup?
+        
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        Service.shared.fetchGames{(appGroup, err) in
+            dispatchGroup.leave()
+            group1 = appGroup
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
             }
-            
         }
+        
+        dispatchGroup.enter()
+        Service.shared.fetchTopGrossing{(appGroup, err) in
+            dispatchGroup.leave()
+            group2 = appGroup
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+        
+        dispatchGroup.enter()
+        Service.shared.fetchAppGroup(urlString: "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-free/all/25/explicit.json"){(appGroup, err) in
+            dispatchGroup.leave()
+            group3 = appGroup
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main){
+            if let group = group1{
+                self.groups.append(group)
+            }
+            if let group = group2{
+                self.groups.append(group)
+            }
+            if let group = group3{
+                self.groups.append(group)
+            }
+            self.collectionView.reloadData()
+        }
+        
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -43,19 +80,21 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return groups.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId , for: indexPath) as! AppsGroupCell
-        cell.titleLabel.text = editorsChoiceGames?.feed.title
-        cell.horizontalController.appGroup = editorsChoiceGames
+        let appGroups = groups[indexPath.item]
+        
+        cell.titleLabel.text = appGroups.feed.title
+        cell.horizontalController.appGroup = appGroups
         cell.horizontalController.collectionView.reloadData()
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        return .init(width: view.frame.width, height: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
--- Appstore/Service/Service.swift
+++ Appstore/Service/Service.swift
@@ -29,11 +29,19 @@ class Service {
             }.resume()
     }
     
+    func fetchTopGrossing(completion: @escaping (AppGroup?, Error?) ->()){
+        //let url = https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json"
+        let url = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json"
+        fetchAppGroup(urlString: url, completion: completion)
+    }
+    
     func fetchGames(completion: @escaping (AppGroup?, Error?) ->()){
-        //guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json")
-        guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json")
-            
-            else { return }
+        let url = "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/25/explicit.json"
+        fetchAppGroup(urlString: url, completion: completion)
+    }
+    
+    func fetchAppGroup(urlString: String, completion: @escaping (AppGroup?, Error?) ->()){
+        guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
             
             if let err = err{
