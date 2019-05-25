e81a667773525cd10ff5d88b0ffc6bdd71cf4359
Oyegoke Tomisin <tomisin18@gmail.com>
2019-05-25 22:18:39 +0100

    iTunes Reviews JSON

--- Appstore/Controller/Apps/Details/ReviewsController.swift
+++ Appstore/Controller/Apps/Details/ReviewsController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ReviewsController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
+    var reviews: Reviews?{
+        didSet{
+            collectionView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +26,15 @@ class ReviewsController: HorizontalSnappingController, UICollectionViewDelegateF
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return reviews?.feed.entry.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ReviewCell
-       
+        let entry = reviews?.feed.entry[indexPath.item]
+        cell.authorLabel.text = entry?.author.name.label
+        cell.titleLabel.text = entry?.title.label
+        cell.bodyLabel.text = entry?.content.label
         return cell
     }
     
