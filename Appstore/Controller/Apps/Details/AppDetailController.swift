607f45f9fbd46ae5be7f03e4c3d6dbf96f396af0
Oyegoke Tomisin <tomisin18@gmail.com>
2019-05-25 14:41:19 +0100

    Preview Screenshots Cells

--- Appstore/Controller/Apps/Details/AppDetailController.swift
+++ Appstore/Controller/Apps/Details/AppDetailController.swift
@@ -1,42 +1,76 @@
 //
-//  AppDetailsController.swift
-//  Appstore
+//  AppDetailController.swift
+//  AppStoreJSONApis
 //
-//  Created by OYEGOKE TOMISIN on 24/05/2019.
-//  Copyright © 2019 com.OyegokeTomisin. All rights reserved.
+//  Created by Brian Voong on 2/19/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
 //
 
 import UIKit
 
 class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayout {
     
-    fileprivate let detailCellId = "detailCellId"
-    var appId: String!{
-        didSet{
-            print("here is my app id", appId)
+    var appId: String! {
+        didSet {
+            print("Here is my appId:", appId)
+            let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
+            Service.shared.fetchGenericJSONData(urlString: urlString) { (result: SearchResult?, err) in
+                let app = result?.results.first
+                self.app = app
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
         }
     }
+    
+    var app: Result?
+    
+    let detailCellId = "detailCellId"
+    let previewCellId = "previewCellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: detailCellId)
+        collectionView.register(PreviewCell.self, forCellWithReuseIdentifier: previewCellId)
         
         navigationItem.largeTitleDisplayMode = .never
-        collectionView.backgroundColor = .white
-        collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: detailCellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
-    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
         
-        return cell
+        if indexPath.item == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
+            cell.app = app
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: previewCellId, for: indexPath) as! PreviewCell
+            cell.horizontalController.app = self.app
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        
+        if indexPath.item == 0 {
+            // calculate the necessary size for our cell somehow
+            let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
+            dummyCell.app = app
+            dummyCell.layoutIfNeeded()
+            
+            let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+            
+            return .init(width: view.frame.width, height: estimatedSize.height)
+        } else {
+            return .init(width: view.frame.width, height: 500)
+        }
+        
     }
     
 }
--- Appstore/Helpers/BetterSnappingLayout.swift
+++ Appstore/Helpers/BetterSnappingLayout.swift
@@ -11,12 +11,24 @@ import UIKit
 class BetterSnappingLayout: UICollectionViewFlowLayout {
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+        
+        guard let collectionView = collectionView else {
+            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        }
+        
+        let nextX: CGFloat
+        
+        if proposedContentOffset.x <= 0 || collectionView.contentOffset == proposedContentOffset {
+            nextX = proposedContentOffset.x
+        } else {
+            nextX = collectionView.contentOffset.x + (velocity.x > 0 ? collectionView.bounds.size.width : -collectionView.bounds.size.width)
+        }
+        
+        let targetRect = CGRect(x: nextX, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
         
         var offsetAdjustment = CGFloat.greatestFiniteMagnitude
-        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
         
-        let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
         
         let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
         
